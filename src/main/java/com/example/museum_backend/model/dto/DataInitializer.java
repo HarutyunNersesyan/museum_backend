c11f08e7b1408fc218443e723184c532efc9c3fa
HARUT_037 <harut.037@mail.ru>
2026-04-17 20:34:22 +0400

    Fist commit.

--- src/main/java/com/example/museum_backend/model/dto/DataInitializer.java
+++ src/main/java/com/example/museum_backend/model/dto/DataInitializer.java
@@ -23,167 +23,132 @@ public class DataInitializer {
 
     @PostConstruct
     public void init() {
+
         if (museumRepository.count() > 0 || eventRepository.count() > 0) {
             return;
         }
 
         // ==================== HISTORY ====================
-        Museum history = saveMuseum("History Museum of Armenia");
+        Museum history = new Museum();
+        history.setName("History Museum of Armenia");
+        history = museumRepository.save(history);
 
-        createEvent(history,
-                "Հնագույն քաղաքակրթություններ",
-                "Այս ցուցադրությունը ներկայացնում է Հայաստանի տարածքում ձևավորված առաջին քաղաքակրթությունները՝ սկսած նախնադարից մինչև հին թագավորությունների ձևավորում։ Այցելուները կարող են տեսնել հնագիտական եզակի գտածոներ, կենցաղային իրեր և մշակութային արժեքներ։",
-                EventCategory.HISTORY, Location.YEREVAN, 30, 10);
+        Event h1 = new Event();
+        h1.setName("Հայաստանի պատմություն");
+        h1.setDescription("Հայաստանի պատմության թանգարանում տեղի կունենա «Դրվագներ ինքնության. գորգ» խորագրով ցուցադրության փակման արարողությունը։\n" +
+                "Միջոցառման ընթացքում կներկայացվեն ցուցադրության վերաբերյալ վիճակագրական տվյալներ, ինչպես նաև թանգարանի գործընկեր «Մեգերյան կարպետ» մշակութային համալիրի հետ համագործակցությամբ ամիսներ շարունակ վերոնշյալ ցուցասրահում գործված գորգի կրկնօրինակը կհամալրի թանգարանի հուշանվերների տեսականին։ Գորգը գործվել է 17-րդ դարով թվագրվող՝ Արցախի մետաքսե ծածկոցի նմանությամբ։ Այս և այլ մանրամասների մասին կխոսի Հայաստանի պատմության թանգարանի Ազգագրության բաժնի գիտաշխատող Լիլիա Ավանեսյանը");
+        h1.setEventCategory(EventCategory.HISTORY);
+        h1.setEventType(EventType.MOBILE);
+        h1.setImageUrls(List.of("http://localhost:8080/uploads/default1.jpg"));
+        h1.setEventDate(buildDate(30, 10, 0));
+        h1.setPhoneNumber("+374 10 520-690");
+        h1.setGuidePrice(1000);
+        h1.setTicketPrice(4000);
+        h1.setLocation(Location.YEREVAN);
+        h1.setDuration(2);
+        h1.setMuseum(history);
+        eventRepository.save(h1);
 
-        createEvent(history,
-                "Հայկական թագավորություններ",
-                "Ցուցադրությունը նվիրված է հայկական թագավորությունների պատմությանը՝ ներկայացնելով նրանց զարգացումը, ռազմական ուժը և մշակութային ժառանգությունը։ Ներկայացված են քարտեզներ, զենքեր և պատմական փաստաթղթեր։",
-                EventCategory.HISTORY, Location.YEREVAN, 31, 12);
+        Event h2 = new Event();
+        h2.setName("Պատմական Վերադարձ");
+        h2.setDescription("Հայաստանի պատմության թանգարանում կբացվի միջնադարյան հայկական փայտարվեստի եզակի ցուցանմուշներից մեկի՝ 1188թ. հայկական եկեղեցու փայտյա դռան փեղկի ցուցադրությունը։\n" +
+                "Բացառիկ ցուցանմուշը գնվել է Լոնդոնի «Սեմ Ֆոգ» ցուցասրահից՝ Հայաստանի Հանրապետության կառավարության որոշմամբ և պետական միջոցներով, Մշակույթի զարգացման հիմնադրամի կողմից։ Այն այսուհետ կհամալրի Հայաստանի պատմության թանգարանի հավաքածուն։");
+        h2.setEventCategory(EventCategory.HISTORY);
+        h2.setEventType(EventType.MOBILE);
+        h2.setImageUrls(List.of("http://localhost:8080/uploads/default2.jpg"));
+        h2.setEventDate(buildDate(31, 12, 0));
+        h2.setPhoneNumber("+374 10 520-690");
+        h2.setGuidePrice(1000);
+        h2.setTicketPrice(3000);
+        h2.setLocation(Location.YEREVAN);
+        h2.setDuration(2);
+        h2.setMuseum(history);
+        eventRepository.save(h2);
 
         // ==================== MATENADARAN ====================
-        Museum matenadaran = saveMuseum("Matenadaran");
+        Museum matenadaran = new Museum();
+        matenadaran.setName("Matenadaran");
+        matenadaran = museumRepository.save(matenadaran);
 
-        createEvent(matenadaran,
-                "Ձեռագրերի գաղտնիքները",
-                "Այս միջոցառումը ներկայացնում է միջնադարյան ձեռագրերի հարուստ աշխարհը՝ ընդգրկելով տարբեր լեզուներով գրված բացառիկ նմուշներ։ Այցելուները կարող են բացահայտել գրերի զարգացումը և մշակութային արժեքը։",
-                EventCategory.HISTORY, Location.YEREVAN, 45, 11);
+        Event m1 = new Event();
+        m1.setName("Ձեռագրերի գաղտնիքները");
+        m1.setDescription("Միջնադարյան ձեռագրերի աշխարհ՝ տարբեր լեզուներով բացառիկ նմուշներ։");
+        m1.setEventCategory(EventCategory.HISTORY);
+        m1.setEventType(EventType.MOBILE);
+        m1.setImageUrls(List.of("http://localhost:8080/uploads/default3.webp"));
+        m1.setEventDate(buildDate(45, 11, 0));
+        m1.setPhoneNumber("+374 10 513-000");
+        m1.setGuidePrice(2000);
+        m1.setTicketPrice(5000);
+        m1.setLocation(Location.YEREVAN);
+        m1.setDuration(2);
+        m1.setMuseum(matenadaran);
+        eventRepository.save(m1);
 
-        createEvent(matenadaran,
-                "Հին գրերի զարգացում",
-                "Ցուցադրությունը ներկայացնում է գրերի պատմական զարգացումը՝ սկսած հին ձեռագրերից մինչև միջնադարյան գրականություն։ Ներկայացված են եզակի նմուշներ և պատմական փաստաթղթեր։",
-                EventCategory.HISTORY, Location.YEREVAN, 46, 13);
+        Museum erebuni = new Museum();
+        erebuni.setName("Erebuni Fortress & Museum");
+        erebuni = museumRepository.save(erebuni);
 
-        // ==================== CAFESJIAN ====================
-        Museum cafesjian = saveMuseum("Cafesjian Center for the Arts");
-
-        createEvent(cafesjian,
-                "Ժամանակակից արվեստ",
-                "Ցուցահանդեսը ներկայացնում է ժամանակակից արվեստի բազմազան ուղղություններ՝ ներառյալ աբստրակտ և փորձարարական գործեր։ Այցելուները հնարավորություն կունենան բացահայտել նորարարական մոտեցումներ արվեստում։",
-                EventCategory.ART, Location.YEREVAN, 20, 12);
-
-        createEvent(cafesjian,
-                "Քանդակների ցուցադրություն",
-                "Այս ցուցադրությունը ներկայացնում է հայկական և միջազգային քանդակագործների աշխատանքները՝ ընդգծելով ժամանակակից արվեստի նոր ձևերը և արտահայտչամիջոցները։",
-                EventCategory.ART, Location.YEREVAN, 21, 14);
-
-        // ==================== EREBUNI ====================
-        Museum erebuni = saveMuseum("Erebuni Fortress & Museum");
-
-        createEvent(erebuni,
-                "Էրեբունի պատմություն",
-                "Ցուցադրությունը նվիրված է Էրեբունի ամրոցի հիմնադրմանը և ուրարտական մշակույթին։ Այստեղ ներկայացված են հնագիտական գտածոներ, որմնանկարներ և պատմական փաստեր։",
-                EventCategory.ARCHAEOLOGY, Location.YEREVAN, 60, 13);
-
-        createEvent(erebuni,
-                "Ուրարտու մշակույթ",
-                "Այս միջոցառումը ներկայացնում է Ուրարտու թագավորության մշակույթը, տնտեսությունը և ռազմական համակարգը՝ ցուցադրելով եզակի հնագիտական նմուշներ։",
-                EventCategory.ARCHAEOLOGY, Location.YEREVAN, 61, 15);
+        Event e1 = new Event();
+        e1.setName("Էրեբունի պատմություն");
+        e1.setDescription("Ուրարտու և Էրեբունի ամրոցի պատմություն։");
+        e1.setEventCategory(EventCategory.ARCHAEOLOGY);
+        e1.setEventType(EventType.MOBILE);
+        e1.setImageUrls(List.of("http://localhost:8080/uploads/default6.jpg"));
+        e1.setEventDate(buildDate(60, 13, 0));
+        e1.setPhoneNumber("+374 10 461-393");
+        e1.setGuidePrice(1000);
+        e1.setTicketPrice(2000);
+        e1.setLocation(Location.YEREVAN);
+        e1.setDuration(2);
+        e1.setMuseum(erebuni);
+        eventRepository.save(e1);
 
         // ==================== GENOCIDE ====================
-        Museum genocide = saveMuseum("Armenian Genocide Museum");
+        Museum genocide = new Museum();
+        genocide.setName("Armenian Genocide Museum");
+        genocide = museumRepository.save(genocide);
 
-        createEvent(genocide,
-                "Հիշատակ",
-                "Ցուցադրությունը նվիրված է Հայոց ցեղասպանության զոհերի հիշատակին՝ ներկայացնելով պատմական փաստեր, վավերագրեր և անձնական պատմություններ։",
-                EventCategory.HISTORY, Location.YEREVAN, 90, 14);
+        Event g1 = new Event();
+        g1.setName("Հիշատակ");
+        g1.setDescription("Ցեղասպանության զոհերի հիշատակին նվիրված ցուցադրություն։");
+        g1.setEventCategory(EventCategory.HISTORY);
+        g1.setEventType(EventType.MOBILE);
+        g1.setImageUrls(List.of("http://localhost:8080/uploads/default4.jpg"));
+        g1.setEventDate(buildDate(90, 14, 0));
+        g1.setPhoneNumber("+374 10 390-980");
+        g1.setGuidePrice(1000);
+        g1.setTicketPrice(4000);
+        g1.setLocation(Location.YEREVAN);
+        g1.setDuration(3);
+        g1.setMuseum(genocide);
+        eventRepository.save(g1);
 
-        createEvent(genocide,
-                "Վերածնունդ",
-                "Այս միջոցառումը ներկայացնում է հայ ժողովրդի վերածնունդը ցեղասպանությունից հետո՝ ընդգծելով մշակութային և ազգային ինքնության պահպանման կարևորությունը։",
-                EventCategory.HISTORY, Location.YEREVAN, 91, 16);
+        Museum cafesjian = new Museum();
+        cafesjian.setName("Cafesjian Center for the Arts");
+        cafesjian = museumRepository.save(cafesjian);
 
-        // ==================== DILIJAN ====================
-        Museum dilijan = saveMuseum("Dilijan Local Lore Museum");
+        // ==================== ADDED MUSEUMS ====================
 
-        createEvent(dilijan,
-                "Բնություն",
-                "Ցուցադրությունը ներկայացնում է Դիլիջանի բնական միջավայրը, բուսական և կենդանական աշխարհը՝ ընդգծելով էկոհամակարգերի կարևորությունը։",
-                EventCategory.NATURAL_HISTORY, Location.DILIJAN, 40, 15);
+        Museum dilijan = new Museum();
+        dilijan.setName("Dilijan Local Lore Museum");
+        dilijan = museumRepository.save(dilijan);
 
-        createEvent(dilijan,
-                "Մշակույթ",
-                "Այս միջոցառումը ներկայացնում է Դիլիջանի մշակութային ժառանգությունը, ավանդույթներն ու տեղական արհեստները։",
-                EventCategory.NATURAL_HISTORY, Location.DILIJAN, 41, 17);
+        Museum gyumri = new Museum();
+        gyumri.setName("Gyumri Museum of Architecture");
+        gyumri = museumRepository.save(gyumri);
 
-        // ==================== MEGERIAN ====================
-        Museum megerian = saveMuseum("Megerian Carpet Museum");
+        Museum khorVirap = new Museum();
+        khorVirap.setName("Khor Virap Museum");
+        khorVirap = museumRepository.save(khorVirap);
 
-        createEvent(megerian,
-                "Գորգերի արվեստ",
-                "Ցուցադրությունը ներկայացնում է հայկական գորգագործության պատմությունն ու զարգացումը՝ ցուցադրելով տարբեր ժամանակաշրջանների նմուշներ։",
-                EventCategory.CULTURAL, Location.YEREVAN, 55, 16);
+        Museum parajanov = new Museum();
+        parajanov.setName("Sergey Parajanov Museum");
+        parajanov = museumRepository.save(parajanov);
 
-        createEvent(megerian,
-                "Վարպետության դաս",
-                "Այցելուները կարող են մասնակցել գորգագործության վարպետության դասերի՝ ծանոթանալով ավանդական տեխնիկաներին և նյութերին։",
-                EventCategory.CULTURAL, Location.YEREVAN, 56, 18);
-
-        // ==================== PARAJANOV ====================
-        Museum parajanov = saveMuseum("Sergey Parajanov Museum");
-
-        createEvent(parajanov,
-                "Կոլաժներ",
-                "Ցուցադրությունը ներկայացնում է Սերգեյ Փարաջանովի յուրահատուկ կոլաժները՝ արտահայտելով նրա ստեղծագործական աշխարհը։",
-                EventCategory.ART, Location.YEREVAN, 25, 17);
-
-        createEvent(parajanov,
-                "Արվեստի աշխարհ",
-                "Այս միջոցառումը բացահայտում է Փարաջանովի արվեստի բազմաշերտ աշխարհը՝ ներառելով գծանկարներ և անձնական իրեր։",
-                EventCategory.ART, Location.YEREVAN, 26, 19);
-
-        // ==================== KHOR VIRAP ====================
-        Museum khorVirap = saveMuseum("Khor Virap Museum");
-
-        createEvent(khorVirap,
-                "Քրիստոնեություն",
-                "Ցուցադրությունը ներկայացնում է Հայաստանում քրիստոնեության ընդունման պատմությունը և դրա նշանակությունը ազգային ինքնության համար։",
-                EventCategory.RELIGIOUS, Location.ARARAT, 70, 18);
-
-        createEvent(khorVirap,
-                "Սրբավայր պատմություն",
-                "Այս միջոցառումը ներկայացնում է Խոր Վիրապի պատմությունը՝ որպես կարևոր հոգևոր կենտրոն և ուխտատեղի։",
-                EventCategory.RELIGIOUS, Location.ARARAT, 71, 19);
-
-        // ==================== GYUMRI ====================
-        Museum gyumri = saveMuseum("Gyumri Museum of Architecture");
-
-        createEvent(gyumri,
-                "Ճարտարապետություն",
-                "Ցուցադրությունը ներկայացնում է Գյումրու ճարտարապետական ժառանգությունը՝ ընդգծելով 19-րդ դարի շենքերը և քաղաքային միջավայրը։",
-                EventCategory.HISTORY, Location.GYUMRI, 35, 19);
-
-        createEvent(gyumri,
-                "Հին շենքեր",
-                "Այս միջոցառումը ներկայացնում է Գյումրու հին շենքերի պատմությունը և դրանց պահպանման կարևորությունը։",
-                EventCategory.HISTORY, Location.GYUMRI, 36, 20);
-    }
-
-    private Museum saveMuseum(String name) {
-        Museum museum = new Museum();
-        museum.setName(name);
-        return museumRepository.save(museum);
-    }
-
-    private void createEvent(Museum museum, String name, String description,
-                             EventCategory category, Location location,
-                             int days, int hour) {
-
-        Event event = new Event();
-        event.setName(name);
-        event.setDescription(description);
-        event.setEventCategory(category);
-        event.setEventType(EventType.MOBILE);
-        event.setImageUrls(List.of("http://localhost:8080/uploads/default.jpg"));
-        event.setEventDate(buildDate(days, hour, 0));
-        event.setPhoneNumber("+37400000000");
-        event.setContactEmail("info@museum.am");
-        event.setGuidePrice(1000);
-        event.setTicketPrice(2000);
-        event.setLocation(location);
-        event.setDuration(2);
-        event.setMuseum(museum);
-
-        eventRepository.save(event);
+        Museum megerian = new Museum();
+        megerian.setName("Megerian Carpet Museum");
+        megerian = museumRepository.save(megerian);
     }
 
     private LocalDateTime buildDate(int days, int hour, int minute) {
